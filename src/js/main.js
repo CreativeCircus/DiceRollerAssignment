7d34a99e28580f53ecad251f64981d62329ae27f
Christopher Silich <chris.silich@gmail.com>
2020-11-11 15:04:51 -0500

    clean up, blanked readme

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
-console.log(`Hello World from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`);
-
 let socket = io('http://circuslabs.net:20202');
 
+// how to ask the server for a roll
 socket.emit('request roll', {
 	count: 1,
 	faces: 6,
 	name: 'chris',
 });
 
+// when the server provides a roll for ANYONE (not just you), this is called
 socket.on('rolled', function (data) {
 	console.log(data);
 });
